be7a0d08b26a2080ed563e8ec47e40eba9396314
amdomanska <aga@resin.io>
2018-09-13 15:25:52 +0100

    work break

---
+++ lib/gui/app/components/drive-selector-button/drive-selector-button.jsx
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2016 resin.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 'use strict'
+
+const React = require('react')
+const ReactDOM = require('react-dom')
+const propTypes = require('prop-types')
+const Color = require('color')
+
+const middleEllipsis = require('./../../utils/middle-ellipsis')
+
+import styled, { ThemeProvider } from 'styled-components'
+import { Button, Txt, Box, Provider } from 'rendition'
+
+import shared from '/./../../../../../lib/shared/units.js'
+import consts from './../../scss/main'
+import theme from './../../scss/modules/_theme'
+
+const StyledButton = styled(Button)`
+  width: 100%;
+  max-width: ${theme.colors.$btnMinWidth};
+  margin: auto;
+
+  &:disabled {
+    background-color: ${theme.colors.$paletteThemeDarkDisabledBackground};
+    color: ${theme.colors.$paletteThemeDarkDisabledForeground};
+    &:hover {
+      background-color: ${theme.colors.$paletteThemeDarkDisabledBackground};
+      color: ${theme.colors.$paletteThemeDarkDisabledForeground};
+    }
+  }
+
+  overflow: hidden;
+`
+
+const DriveNameButton = styled(Button).attrs({
+  className: 'step-image step-name'
+})`
+  &:hover {
+    color: ${theme.colors.$paletteThemePrimaryForeground};
+  }
+
+  &:focus {
+    color: ${theme.colors.$paletteThemePrimaryForeground};
+  }
+
+  &:active {
+    color: ${theme.colors.$paletteThemePrimaryForeground};
+  }
+`
+
+const ChangeButton = styled(Button).attrs({
+  className: 'button button-link step-footer'
+})``
+
+const StyledText = styled(Txt).attrs({
+  className: 'step-image step-size'
+})``
+
+class DriveSelectorButton extends React.Component {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      drive: ''
+    }
+  }
+
+  static getDerivedStateFromProps (props, state) {
+    state.drive = !props.hasDrive() && props.shouldShowDrivesButton()
+  }
+
+
+  render () {
+    if (this.state.drive){
+      return (
+        <Provider>
+          <StyledButton
+            primary
+            disabled={this.props.disabled()}
+            // onClick={() => this.props.openImageSelector()}
+          >
+            Select drive
+          </StyledButton>
+        </Provider>
+      )
+    }
+    else {
+      return (
+        <Provider>
+          <DriveNameButton
+            plaintext
+            // onClick={() => this.props.showSelectedImageDetails()}
+            // tooltip={this.props.getImageBasename}
+          >
+            //{ this.state.chosenImage}
+          </DriveNameButton>
+          <StyledText>
+            //{ this.state.imageSize}
+          </StyledText>
+          { this.props.flashing ?
+            null
+            :
+            <ChangeButton
+              plaintext
+              // onClick={() => this.props.reselectImage()}
+            >
+              Change
+            </ChangeButton>
+          }
+        </Provider>
+      )
+    }
+  }
+}
+
+DriveSelectorButton.propTypes = {
+  hasDrive: propTypes.func,
+  shouldShowDrivesButton: propTypes.func,
+  disabled: propTypes.func,
+  // image: propTypes.func,
+  // showSelectedImageDetails: propTypes.func,
+  // getImageName: propTypes.func,
+  // getImageBasename: propTypes.func,
+  // reselectImage: propTypes.func,
+  // flashing: propTypes.func,
+  // getImageSize: propTypes.func,
+}
+
+module.exports = DriveSelectorButton
